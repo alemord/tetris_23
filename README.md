a0194fb4f53a28bc05877bb7007d60910b5fc25f
alemord <alessandro.mordini@gmail.com>
2023-03-01 15:41:59 -0500

    edited css and building js

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # myTetris2023
-# tetris_23
+### This is a HTML, CSS and JavaScript code for a classic Tetris game. This code creates a grid, generates geometric shapes and allows the user to move pieces around within the grid.
+### I created a variable grid that holds the div elements (200) with a class of grid. The scoreDisplay variable holds the element with an ID of score, and startBtn holds the element with an ID of start-button. 
+### The code then defines five geometric shapes, each one with a different color and four different rotation options.
+
--- index.js
+++ index.js
@@ -1,11 +1,54 @@
-const PIECES = [  [[0, 0], [0, 1], [1, 0], [1, 1]],  // square
-  [[0, -1], [0, 0], [0, 1], [0, 2]], // line
-  [[0, -1], [0, 0], [1, 0], [1, 1]], // L-shape
-  [[0, 1], [0, 0], [1, 0], [1, -1]], // inverse L-shape
-  [[0, 0], [0, 1], [0, -1], [1, 0]], // T-shape
-  [[0, 0], [0, 1], [0, -1], [1, -1]],// S-shape
-  [[0, 0], [0, -1], [0, 1], [1, 1]] // Z-shape
-];
+  //colors of the pieces
+  const colors = [
+    'purple',
+    'green',
+    'blue',
+    'orange',
+    'red'
+  ]
+
+  //pieces
+  const lShape = [
+    [1, width+1, width*2+1, 2],
+    [width, width+1, width+2, width*2+2],
+    [1, width+1, width*2+1, width*2],
+    [width, width*2, width*2+1, width*2+2]
+  ]
+
+  const zShape = [
+    [0,width,width+1,width*2+1],
+    [width+1, width+2,width*2,width*2+1],
+    [0,width,width+1,width*2+1],
+    [width+1, width+2,width*2,width*2+1]
+  ]
+
+  const tShape = [
+    [1,width,width+1,width+2],
+    [1,width+1,width+2,width*2+1],
+    [width,width+1,width+2,width*2+1],
+    [1,width,width+1,width*2+1]
+  ]
+
+  const oShape = [
+    [0,1,width,width+1],
+    [0,1,width,width+1],
+    [0,1,width,width+1],
+    [0,1,width,width+1]
+  ]
+
+  const iShape = [
+    [1,width+1,width*2+1,width*3+1],
+    [width,width+1,width+2,width+3],
+    [1,width+1,width*2+1,width*3+1],
+    [width,width+1,width+2,width+3]
+  ]
+
+  const theTetrominoes = [lShape, zShape, tShape, oShape, iShape]
+
+  let currentPosition = 4
+  let currentRotation = 0
+
+  console.log(theTetrominoes[0][0])
 
 //initialize board  
 class Game {
@@ -14,10 +57,3 @@ class Game {
     this.currentPiece = null;  
     this.score = 0;  
   }
-const colors = [
-  'orange',
-  'red',
-  'purple',
-  'green',
-  'blue'
-]
